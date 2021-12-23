3a3346f7103a901d3a7e4bb6cb2b0ab1296878aa
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-12-23 17:40:12 +0100

    add: user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
+    @previews = []
+    # set up chatroom preview
+    @user.chatrooms.each do |room|
+      other_user = room.reject{ |user| user == current_user }[0]
+      preview[:room] = room
+      preview[:user] = other_user
+      preview[:last_message] = room.messages.last
+      @previews << preview
+    end
   end
 end
